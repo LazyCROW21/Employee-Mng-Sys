e8df0d2705608cd44d9f61f529ca5987890bc8eb
LazyCROW <hardikkardam21@gmail.com>
2022-01-12 21:13:00 +0530

    Update server.test.js

--- server/server.test.js
+++ server/server.test.js
@@ -9,7 +9,6 @@ describe("GET /employee", () => {
             expect(response.statusCode).toBe(200);
         });
 
-        // test("should return json array & header", ()=>{});
         test("should return json array & header", async () => {
             const response = await request.get("/api/employee").send();
             expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
@@ -27,14 +26,14 @@ describe("GET /employee", () => {
         });
     });
     describe("When id parameter is passed", () => {
-        const response = await request.get("/api/employee/1").send();
-        var Obj = response.body;
         test("should respond with status code 200", async () => {
+            const response = await request.get("/api/employee/1").send();
             expect(response.statusCode).toBe(200);
         });
-
+        
         test("should return json object & header", async () => {
-            // const response = await request.get("/api/employee/1").send();
+            const response = await request.get("/api/employee/1").send();
+            var Obj = response.body;
             expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
             var isObj = false;
             try {
@@ -49,9 +48,9 @@ describe("GET /employee", () => {
         });
 
         test("object contains the required field ", async () => {
-            // const response = await request.get("/api/employee/1").send();
-            // expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
-            var isValidObj = true;
+            const response = await request.get("/api/employee/1").send();
+            // var isValidObj = true;
+            var Obj = response.body;
             var reqField = [
                 'id', 
                 'first_name',
@@ -66,25 +65,126 @@ describe("GET /employee", () => {
                 // var Obj = response.body;
                 if (typeof Obj === 'object' && Obj !== null) {
                     for (let i = 0; i < reqField.length; i++) {
-                        if (!Obj[reqField[i]]) {
-                            isValidObj = false;
-                            break;
-                        }
+                        expect(Obj[reqField[i]]).toBeDefined()
+                        // if (!Obj[reqField[i]]) {
+                        //     isValidObj = false;
+                        //     break;
+                        // }
                     }
                 }
             } catch (err) {
                 console.log(err);
-                isValidObj = false;
+                // isValidObj = false;
             }
-            expect(isValidObj).toBe(true);
+            // expect(isValidObj).toBe(true);
         });
     });
     describe("When invalid parameter is passed", ()=>{
-        const response = await request.get("/api/employee/wrongid").send();
-        // var Obj = response.body;
-        test("should respond with status code 400", ()=>{
+        test("should respond with status code 400", async()=>{
+            const response = await request.get("/api/employee/wrongid").send();
+            expect(response.statusCode).toBe(400);
+        });
+    });
+});
+
+describe("POST /employee", () => {
+    describe("When correct data is passed", () => {
+        test("should respond with status code 200", async () => {
+            var Obj = {
+                "first_name": "Opal",
+                "last_name": "Raycroft",
+                "phone": "8234802021",
+                "email": "OpalRaycroft@columbia.edu",
+                "dept_id": 1,
+                "designation_id": 1,
+                "salary": 12000
+            };
+            const response = await request.post("/api/employee").send(Obj);
             expect(response.statusCode).toBe(200);
         });
-        // test("should return json object & header", ()=>{});
+
+        test("should return json header", async () => {
+            var Obj = {
+                "first_name": "Opal",
+                "last_name": "Raycroft",
+                "phone": "8234802021",
+                "email": "asdsaq@asd.edu",
+                "dept_id": 1,
+                "designation_id": 1,
+                "salary": 12000
+            };
+            const response = await request.post("/api/employee").send(Obj);
+            expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+        });
+
+        test("should return inserted json object with id", async () => {
+            var Obj = {
+                "first_name": "Opal",
+                "last_name": "Raycroft",
+                "phone": "8234802021",
+                "email": "qqqeq@asd.edu",
+                "dept_id": 1,
+                "designation_id": 1,
+                "salary": 12000
+            };
+            const response = await request.post("/api/employee").send(Obj);
+            var respObj = response.body;
+            var isObj = false;
+            if (typeof respObj === 'object' && respObj !== null) {
+                isObj = true;
+            }
+            expect(isObj).toBe(true);
+            expect(respObj.id).toBeDefined();
+        });
+    });
+    describe("When invalid data is passed", () => {
+        test("should respond with status code 400", async () => {
+            var Obj = {
+                "first_name": "ASDADQ",
+                "last_name": null,
+                "phone": "823as02021",
+                "email": "not an email",
+                "dept_id": -1,
+                "designation_id": 1,
+                "salary": null
+            };
+            const response = await request.post("/api/employee").send(Obj);
+            expect(response.statusCode).toBe(400);
+        });
+    });
+});
+
+describe("DELETE /employee/:id", () => {
+    describe("When valid id data is passed", () => {
+        test("should respond with status code 200", async () => {
+            var id = 14;
+            const response = await request.delete("/api/employee/"+id).send();
+            expect(response.statusCode).toBe(200);
+        });
+
+        test("should return json header", async () => {
+            var id = 12;
+            const response = await request.delete("/api/employee/"+id).send();
+            expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+        });
+
+        test("should return deleted json object with id", async () => {
+            var id = 15;
+            const response = await request.delete("/api/employee/"+id).send();
+            var respObj = response.body;
+            var isObj = false;
+            if (typeof respObj === 'object' && respObj !== null) {
+                isObj = true;
+            }
+            expect(isObj).toBe(true);
+            expect(respObj.id).toBeDefined();
+        });
+    });
+    describe("When invalid data is passed", () => {
+        test("should respond with status code 400", async () => {
+            var id = 'asdad';
+            const response = await request.delete("/api/employee/"+id).send();
+            expect(response.statusCode).toBe(400);
+        });
     });
 });
